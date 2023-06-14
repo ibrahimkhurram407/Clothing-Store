bfea246c42b95a44760e5b8cba67dff4e1689eb8
ibrahimkhurram407 <88876165+ibrahimkhurram407@users.noreply.github.com>
2023-06-14 14:32:53 +0500

    Featured products and Home remedies done
    Responsiveness of webpage is close to 0%

--- bundle/scripts/home-frontEnd.js
+++ bundle/scripts/home-frontEnd.js
@@ -1,43 +1,42 @@
-// //Accent location interpreter for about us section
+//Accent location interpreter for about us section
 
-// // Function to set the position of the image based on container coordinates
-// function setPosition() {
-//   // Get the container and image elements
-//   var container = $(".aboutUs > div div:nth-child(1)");
-//   var image = $(".aboutUs > div:nth-child(1) img");
-//   var containerOffset = container.offset();
-//   var containerTop = containerOffset.top;
-//   var containerLeft = containerOffset.left;
-//   var imageOffset = image.offset();
-//   var imageTop = imageOffset.top;
-//   var imageLeft = imageOffset.left;
-//   var Top = containerTop - imageTop;
-//   var Left = containerLeft - imageLeft;
+// Function to set the position of the image based on container coordinates
+function setPosition() {
+  // Get the container and image elements
+  var container = $(".aboutUs > div div:nth-child(1)");
+  var image = $(".aboutUs > div:nth-child(1) img");
+  var containerOffset = container.offset();
+  var containerTop = containerOffset.top;
+  var containerLeft = containerOffset.left;
+  var imageOffset = image.offset();
+  var imageTop = imageOffset.top;
+  var imageLeft = imageOffset.left;
+  var Top = containerTop - imageTop;
+  var Left = containerLeft - imageLeft;
 
-//   // Set the position of the image relative to the container
-//   image.css({
-//     position: "relative",
-//     top: Top,
-//     left: Left,
-//   });
-// }
+  // Set the position of the image relative to the container
+  image.css({
+    position: "relative",
+    top: Top,
+    left: Left,
+  });
+}
 
-// $(window).on("load resize", function () {
-//     setTimeout(setPosition(), 800);
-//   });
+$(window).on("load resize", function () {
+    setTimeout(setPosition(), 800);
+  });
 
 
-var splide = new Splide( '.splide', {
-  type   : 'loop',
+var splide = new Splide('.splide', {
+  type: 'loop',
   perPage: 3,
   perMove: 1,
-  focus  : 0,
-  gap    : "20px",
-  drag   : "none",
-  snap   : true,
+  focus: 0,
+  gap: "20px",
+  drag: "none",
+  snap: true,
   autoplay: true,
-  
   omitEnd: true,
-} );
+});
 
 splide.mount();
