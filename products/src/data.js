7e268fc11527523093d1b57c797809e9953fabbe
ibrahimkhurram407 <88876165+ibrahimkhurram407@users.noreply.github.com>
2023-07-05 00:03:08 +0500

    Nav Bar Left; Comparison Done
    Updated src.js of comparison page (products page) and updated hrefs of nav bar of check out page
    
    nav still is not responsive

---
+++ products/src/data.js
@@ -0,0 +1,78 @@
+var product = [{
+    "id": 0,
+    "title": "Khan-el-Kaser Body Lotion",
+    "price": 249,
+    "description": "Khan-el-Kaser Body Lotion is a luxurious and hydrating lotion that nourishes and moisturizes the skin. It is enriched with essential oils and natural extracts to leave the skin feeling soft, smooth, and rejuvenated. This body lotion absorbs quickly and has a pleasant fragrance. It is suitable for all skin types and can be used daily for optimal results.",
+    "brand": "Khan-el-Kaser",
+    "image": "../bundle/images/product1.png"
+    }, {
+    "id": 1,
+    "title": "Serie Expert",
+    "price": 299,
+    "description": "Serie Expert is a professional hair care range that offers salon-quality products for various hair concerns. From shampoos and conditioners to treatments and styling products, Serie Expert provides targeted solutions to nourish, repair, and enhance the hair. Whether you have dry, damaged, colored, or frizzy hair, Serie Expert has a product to meet your specific needs. With advanced formulas and high-quality ingredients, Serie Expert helps you achieve healthy, beautiful hair with a luxurious salon experience.",
+    "brand": "L'Oreal",
+    "image": "../bundle/images/product2.png"
+    }, {
+    "id": 2,
+    "title": "Argan Oil of Morocco",
+    "price": 199,
+    "description": "Argan Oil of Morocco is a premium hair oil that provides deep nourishment and hydration to dry and damaged hair. Enriched with pure argan oil, this lightweight and non-greasy formula penetrate the hair shaft to repair and restore its natural shine and softness. It helps to reduce frizz, tame flyaways, and protect the hair from heat styling and environmental damage. Argan Oil of Morocco is suitable for all hair types and can be used as a leave-in treatment or as a finishing oil for added smoothness and luster.",
+    "brand": "Argan Oil of Morocco",
+    "image": "../bundle/images/product3.png"
+    }, {
+    "id": 3,
+    "title": "Hemani Hair Oil",
+    "price": 149,
+    "description": "Hemani Hair Oil is a nourishing blend of natural oils that promotes healthy hair growth and improves hair texture. This oil is enriched with ingredients like olive oil, almond oil, coconut oil, and herbal extracts known for their beneficial properties. It deeply conditions the hair, reduces hair fall, and adds shine and luster. Hemani Hair Oil can be used as a pre-shampoo treatment, a leave-in conditioner, or a scalp massage oil. Regular use of this hair oil will help you achieve strong, silky, and beautiful hair.",
+    "brand": "Hemani",
+    "image": "../bundle/images/product4.png"
+    }, {
+    "id": 4,
+    "title": "Petrova Natural Face Mask",
+    "price": 99,
+    "description": "Petrova Natural Face Mask is a rejuvenating and purifying mask that revitalizes the skin and improves its overall appearance. Made with natural ingredients like clay, botanical extracts, and essential oils, this face mask helps to remove impurities, unclog pores, and reduce excess oil. It leaves the skin feeling refreshed, smooth, and radiant. Petrova Natural Face Mask is suitable for all skin types and can be used weekly as part of your skincare routine to maintain a healthy and glowing complexion.",
+    "brand": "Petrova",
+    "image": "../bundle/images/product5.png"
+    }, {
+    "id": 5,
+    "title":"Shea Butter Hair Masks",
+    "price": 179,
+    "description": "Shea Butter Hair Masks are deeply nourishing treatments that provide intense hydration and repair to dry and damaged hair. Enriched with shea butter, a natural emollient, these hair masks penetrate the hair shaft to moisturize and strengthen the strands. They help to improve hair elasticity, reduce breakage, and restore shine and softness. Shea Butter Hair Masks are suitable for all hair types, especially for those with frizzy, brittle, or chemically treated hair. Regular use of these masks will leave your hair feeling revitalized, manageable, and healthy-looking.",
+    "brand": "Shea Butter",
+    "image": "../bundle/images/product6.png"
+    }, {
+    "id": 6,
+    "title": "Pantene Hair Masks",
+    "price": 129,
+    "description": "Pantene Hair Masks are luxurious treatments that nourish and repair the hair from within. These masks are formulated with Pro-Vitamin B5 and other conditioning agents to deeply hydrate and strengthen the hair fibers. They help to replenish moisture, reduce frizz, and enhance hair manageability. Pantene Hair Masks are suitable for all hair types and can be used weekly to maintain healthy and beautiful hair. With their rich and creamy texture, these masks provide a spa-like experience and leave your hair feeling silky-smooth and rejuvenated.",
+    "brand": "Pantene",
+    "image": "../bundle/images/product7.png"
+    }, {
+    "id": 7,
+    "title": "Dove Conditioner",
+    "price": 89,
+    "description": "Dove Conditioner is a nourishing and hydrating conditioner that leaves your hair soft, smooth, and manageable. It is enriched with a blend of moisturizers and hair-loving ingredients that penetrate deep into the hair strands, providing intense hydration and repair. This conditioner helps to detangle knots, reduce frizz, and improve hair texture. It is suitable for all hair types and can be used daily after shampooing for optimal results. Dove Conditioner restores the natural beauty of your hair, leaving it visibly healthy and beautiful.",
+    "brand": "Dove",
+    "image": "../bundle/images/product8.png"
+    }, {
+    "id": 8,
+    "title": "Remington Coconut Smooth Hair Dryer D5901",
+    "price": 299,
+    "description": "Remington Coconut Smooth Hair Dryer D5901 is a powerful and versatile hair dryer that combines advanced technology with the nourishing benefits of coconut oil. It features multiple heat and speed settings, allowing you to customize your drying experience. The built-in coconut oil infusion system helps to nourish and protect the hair while drying, reducing frizz and promoting smoother, shinier hair. This hair dryer also includes a concentrator attachment for precise styling. With its ergonomic design and professional performance, the Remington Coconut Smooth Hair Dryer D5901 is a must-have tool for achieving salon-quality results at home.",
+    "brand": "Remington",
+    "image": "../bundle/images/product9.png"
+    }, {
+    "id": 9,
+    "title": "Beurer Style Pro Cordless Hair Straightener, HS20",
+    "price": 199,
+    "description": "Beurer Style Pro Cordless Hair Straightener, HS20, is a convenient and portable hair styling tool that allows you to straighten your hair anytime, anywhere. It features advanced ceramic plates that provide even heat distribution for efficient and gentle straightening. The cordless design offers freedom of movement and makes it perfect for travel. This hair straightener heats up quickly and has adjustable temperature settings to suit your hair type. With its sleekand ergonomic design, the Beurer Style Pro Cordless Hair Straightener, HS20, is comfortable to hold and easy to use. It also features a safety lock to ensure secure storage when not in use. The compact size of this hair straightener makes it ideal for on-the-go touch-ups or styling while traveling. Whether you want sleek and straight hair or stylish curls, the Beurer Style Pro Cordless Hair Straightener, HS20, offers versatile styling options. Experience salon-quality results with the convenience of cordless operation and enjoy smooth, shiny, and beautifully styled hair wherever you are.",
+    "brand": "Beurer",
+    "image": "../bundle/images/product10.png"
+    }, {
+    "id": 10,
+    "title": "4 in 1 Hair Straightener, Curler, Crimper Amazon Iron",
+    "price": 149,
+    "description": "The 4 in 1 Hair Straightener, Curler, Crimper Amazon Iron is a versatile styling tool that allows you to create various hairstyles with ease. It features interchangeable plates for straightening, curling, and crimping, giving you the flexibility to switch between different looks. The ceramic tourmaline plates ensure even heat distribution and minimize heat damage to the hair. With adjustable temperature settings, you can customize the heat level to suit your hair type and styling needs. This hair iron heats up quickly and provides long-lasting results. Its compact and lightweight design makes it travel-friendly and convenient to use. Whether you want silky straight hair, bouncy curls, or textured crimps, the 4 in 1 Hair Straightener, Curler, Crimper Amazon Iron has you covered.",
+    "brand": "Amazon",
+    "image": "../bundle/images/product11.png"
+    }];
